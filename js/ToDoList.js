f0ea334a6478cb36a3b9fb3ff3dd7ce8b826b11a
viji-i2i <viji@ideas2it.com>
2019-10-09 20:47:32 +0530

    Features Functionality to add Lists is Included

--- js/ToDoList.js
+++ js/ToDoList.js
@@ -1,17 +1,30 @@
-//document.getElementById('add-list').addEventListener('click',addNewTask);
-document.getElementById("show-text-field").addEventListener("click", showTextField);
+document.getElementById("add-list").addEventListener("click",addNewList);
+document.getElementById("show-text-field").addEventListener("click", showRightSideBar);
+document.getElementById("show-right-side-bar").addEventListener("click", showRightSideBar);
+
 var newListInput = document.getElementById('new-list-input');
 var toDoLists = []; 
 var taskId = 0;
 
-function showTextField() {
-  var textField = document.getElementById("text-field");
-  if (textField.style.display === "block") {
-    textField.style.display = "none";
-  } else {
-    textField.style.display = "block";
+function showRightSideBar() {
+  var textField = document.getElementsByClassName("right-side-bar");
+  for(let index in textField) {
+    if (textField[index].style.display === "block") {
+      textField[index].style.display = "none";
+    } else {
+      textField[index].style.display = "block";
+    }
   }
 }
+/* To Trigger add-list button While Entering Enter Key */
+document.getElementById("new-list-input")
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode == 13) {
+        document.getElementById("add-list").click();
+    }
+});
+
 /* This Method addNewList is used to store new List items in Array */
 function addNewList() {
   console.log("newList");
@@ -23,18 +36,19 @@ function addNewList() {
   };
   newList.id = taskId++;
   var inputValue = document.getElementById("new-list-input").value;
-  if(inputValue === "") {
-    alert("Enter Your List name");
+  if (inputValue === "") {
+    alert("It seems like List Name is Emplty. Enter Your List name");
   } else {
-  newList.name = inputValue;
-  console.log(newList);
-  toDoLists.push(newList);
-  /* To display All the Available List Items */
-  var li = document.createElement("li");
-  var task = document.createTextNode(inputValue);
-  li.appendChild(task);
-  document.getElementById("list-items").appendChild(li);  
-  document.getElementById("new-list-input").value = "";
+    newList.name = inputValue;
+    console.log(newList);
+    toDoLists.push(newList);
+    /* To display All the Available List Items */
+    var li = document.createElement("li");
+    var task = document.createTextNode(inputValue);
+    li.addEventListener("click", addNewList);
+    li.appendChild(task);
+    document.getElementById("list-items").appendChild(li);  
+    document.getElementById("new-list-input").value = "";
   }
 }
 
